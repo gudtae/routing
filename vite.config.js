5ffe590fed6146d2dae008ae03e9a64e1e08d709
gudtae <90135960+gudtae@users.noreply.github.com>
2023-06-08 14:40:54 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,8 @@
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
 import handlebars from 'vite-plugin-handlebars';
+import { resolve } from 'path';
 
-export default defineConfig({
-    build: {
-        outDir: './static'
-    },
-    plugins: [
-        handlebars({
-          partialDirectory: resolve(__dirname, './src/partials'),
-        }),
-        
-      ],
-
-})
+export default {
+  plugins: [handlebars({
+    partialDirectory: resolve(__dirname, "src/partial")
+  })],
+};
